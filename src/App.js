8271825cfdba62f8219cdc0e610700cba2c1a084
Raghav Vankadara <raghav@Raghavs-MacBook-Air.local>
2023-06-21 12:12:07 +0530

    practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from "react";
+
+function App(){
+   const [open,setOpen] = useState(true);
+   function handleClick(){
+       setOpen(!open);
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+      <div onClick={handleClick} > 
+        <h1>Click Me</h1>
+        {open && <div>Opened</div>}
+      </div>
+
+  );
+  
+}
 export default App;
